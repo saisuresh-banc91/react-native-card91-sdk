3432228f35a40becda86d95c82825e4d6a7139e9
saisuresh388 <75465586+saisuresh-banc91@users.noreply.github.com>
2021-12-20 18:12:59 +0530

    phase 1

--- android/src/main/java/com/reactlibrary/Card91SdkManager.java
+++ android/src/main/java/com/reactlibrary/Card91SdkManager.java
@@ -9,7 +9,12 @@ import androidx.appcompat.widget.AppCompatCheckBox;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import android.content.Intent;
-import com.card91.samplecalculator.*;
+// import com.card91.samplecalculator.*;
+import io.card91.android.sdk.ui.verification.C91VerifyMobileScreen;
+import io.card91.android.sdk.utils.C91Constant;
+import io.card91.android.sdk.utils.SdkEnv;
+
+
 public class Card91SdkManager extends SimpleViewManager<View> {
 
     public static final String REACT_CLASS = "Card91Sdk";
@@ -23,8 +28,13 @@ public class Card91SdkManager extends SimpleViewManager<View> {
     public View createViewInstance(ThemedReactContext c) {
         // TODO: Implement some actually useful functionality
         AppCompatCheckBox cb = new AppCompatCheckBox(c);
-            Intent intent = new Intent(c, CalculatorActivity.class);
+            // Intent intent = new Intent(c, CalculatorActivity.class);
+            // intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            Intent intent = new Intent(c, C91VerifyMobileScreen.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra(C91Constant.ENVIRONMENT, SdkEnv.DEV);
+            intent.putExtra(C91Constant.NUMBER, "919876543213");
+            intent.putExtra(C91Constant.DEVICE_ID, "uiyrdswe218973");
              c.startActivity(intent);
         cb.setChecked(true);
         return cb;
